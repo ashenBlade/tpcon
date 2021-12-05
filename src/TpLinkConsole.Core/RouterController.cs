7b3fe3eb5153952e443298983c8013598de252d3
ashblade <htcdnsnnkom2002@gmail.com>
2021-12-05 12:59:31 +0300

    Add basic functionality for testing connection

--- src/TpLinkConsole.Core/RouterController.cs
+++ src/TpLinkConsole.Core/RouterController.cs
@@ -1,7 +1,43 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace TpLinkConsole.Core
 {
     public class RouterController
     {
-        
+        private readonly Router _router;
+        private readonly HttpClient _client;
+        public RouterController(IPAddress routerAddress, string username, string password)
+        {
+            _router = new Router(routerAddress, username, password);
+            _client = new HttpClient() {BaseAddress = new Uri(routerAddress.ToString()),};
+        }
+
+        private HttpRequestMessage CreateMessage(HttpMethod method)
+        {
+            var message = new HttpRequestMessage()
+                          {
+                              Method = method,
+                              Headers =
+                              {
+                                  Authorization =
+                                      new AuthenticationHeaderValue("Basic",
+                                                                    Convert.ToBase64String(Encoding.UTF8
+                                                                                                   .GetBytes($"{_router.Username}:{_router.Password}"))),
+                                  Referrer = new Uri($"http://{_router.RouterIPAddress}")
+                              }
+                          };
+            return message;
+        }
+
+        public async Task<bool> TestConnectionAsync()
+        {
+            var response = await _client.SendAsync(CreateMessage(HttpMethod.Get));
+            return response.IsSuccessStatusCode;
+        }
     }
 }
