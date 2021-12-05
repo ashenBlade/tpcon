27436f1f5bb4b8e5daeccffb535beed70cc9625c
ashblade <htcdnsnnkom2002@gmail.com>
2021-12-05 13:57:53 +0300

    Add command line parser

---
+++ src/TpLinkConsole.Console/CommandLineUtils/CommandLineArgumentsParser.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TpLinkConsole.Console.CommandLineUtils
+{
+    public class CommandLineArgumentsParser : ICommandLineArgumentsParser
+    {
+        public ICommandLineArguments Parse(string[] args)
+        {
+            var dict = new Dictionary<string, string>();
+            for (int i = 0; i < args.Length; i += 2)
+            {
+                var arg = args[i];
+                if (!IsParameterName(arg))
+                {
+                    throw new ArgumentException($"Parameter name expected. Got: {arg}");
+                }
+
+                string value = null;
+
+                if (i + 1 >= args.Length || IsParameterName(value = args[i + 1]))
+                {
+                    throw new ArgumentException($"Argument expected before {arg}. Got: {value}");
+                }
+
+                dict[arg] = value;
+            }
+
+            return new CommandLineArguments(dict);
+        }
+
+        private bool IsParameterName(string s)
+        {
+            return s.StartsWith("--");
+        }
+    }
+}
