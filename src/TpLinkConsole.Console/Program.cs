a53dfc3566602ccef43e94a6bca5220d9cfe9ba2
ashblade <htcdnsnnkom2002@gmail.com>
2021-12-05 13:06:58 +0300

    Add basic testing connection code with default router arguments

--- src/TpLinkConsole.Console/Program.cs
+++ src/TpLinkConsole.Console/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-System.Console.WriteLine("Hello, World!");
+using System;
+using System.Net;
+
+var controller = new TpLinkConsole.Core.RouterController(IPAddress.Parse("192.168.0.1"), "admin", "admin");
+Console.WriteLine($"Testing connection...");
+var success = await controller.TestConnectionAsync();
+var previousColor = Console.BackgroundColor;
+if (success)
+{
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.WriteLine("Connection exists");
+}
+else
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.WriteLine("Connection failed");
+}
+Console.BackgroundColor = previousColor;
