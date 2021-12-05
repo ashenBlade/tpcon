4f0685107eb5a0cd898e6978a3f2a6a9adb0b457
ashblade <htcdnsnnkom2002@gmail.com>
2021-12-05 13:24:54 +0300

    Refactoring: extract methods and introduced base message object

--- src/TpLinkConsole.Core/RouterController.cs
+++ src/TpLinkConsole.Core/RouterController.cs
@@ -11,24 +11,43 @@ namespace TpLinkConsole.Core
     {
         private readonly Router _router;
         private readonly HttpClient _client;
+        private readonly HttpRequestMessage _baseMessage;
+
         public RouterController(IPAddress routerAddress, string username, string password)
         {
             _router = new Router(routerAddress, username, password);
-            _client = new HttpClient() {BaseAddress = new Uri($"http://{routerAddress.ToString()}"),};
+            _client = new HttpClient() {BaseAddress = new Uri($"http://{routerAddress}"),};
+            _baseMessage = CreateBaseMessage();
         }
 
-        private HttpRequestMessage CreateMessage(HttpMethod method)
+        private string ConvertToBase64(string origin)
         {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(origin));
+        }
+
+        private HttpRequestMessage CreateBaseMessage()
+        {
+            var authHeader = new AuthenticationHeaderValue("Basic", ConvertToBase64($"{_router.Username}:{_router.Password}"));
             var message = new HttpRequestMessage()
                           {
-                              Method = method,
                               Headers =
                               {
-                                  Authorization =
-                                      new AuthenticationHeaderValue("Basic",
-                                                                    Convert.ToBase64String(Encoding.UTF8
-                                                                                                   .GetBytes($"{_router.Username}:{_router.Password}"))),
-                                  Referrer = new Uri($"http://{_router.RouterIPAddress}")
+                                  // Without referer header returns 'Non authorized'
+                                  Referrer = new Uri($"http://{_router.RouterIPAddress}"),
+                                  Authorization = authHeader
+                              }
+                          };
+            return message;
+        }
+
+        private HttpRequestMessage CreateHttpRequestMessage(HttpMethod method, string url)
+        {
+            var message = new HttpRequestMessage(method, url)
+                          {
+                              Headers =
+                              {
+                                  Authorization = _baseMessage.Headers.Authorization,
+                                  Referrer = _baseMessage.Headers.Referrer,
                               }
                           };
             return message;
@@ -36,7 +55,7 @@ namespace TpLinkConsole.Core
 
         public async Task<bool> TestConnectionAsync()
         {
-            var response = await _client.SendAsync(CreateMessage(HttpMethod.Get));
+            var response = await _client.SendAsync(CreateHttpRequestMessage(HttpMethod.Get, "/"));
             return response.IsSuccessStatusCode;
         }
     }
