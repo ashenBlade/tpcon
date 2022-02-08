2c6c6993e9fd26b5f55b4add0edbef7c00281edd
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-08 07:34:07 +0300

    test: Add tests for SubnetMask constructor

--- Router.Domain.Tests/SubnetMaskTests.cs
+++ Router.Domain.Tests/SubnetMaskTests.cs
@@ -17,8 +17,28 @@ public class SubnetMaskTests
     }
 
     [Fact]
-    public void Constructor_WithNegativeSubnetMask_ShouldThrowArgumentOutOfRangeException()
+    public void Constructor_WithLengthMinus7_ShouldThrowArgumentOutOfRangeException()
     {
         Assert.Throws<ArgumentOutOfRangeException>(() => new SubnetMask(-7));
+    }
+
+    [Fact]
+    public void Constructor_WithNegativeMaskLength_ShouldThrowArgumentOutOfRangeException()
+    {
+        var length = Random.Shared.Next(int.MinValue, -1);
+        Assert.Throws<ArgumentOutOfRangeException>(() => new SubnetMask(length));
+    }
+
+    [Fact]
+    public void Constructor_WithLength33_ShouldThrowArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new SubnetMask(33));
+    }
+
+    [Fact]
+    public void Constructor_WithLengthGreaterThan32_ShouldThrowException()
+    {
+        var length = Random.Shared.Next(33, int.MaxValue);
+        Assert.Throws<ArgumentOutOfRangeException>(() => new SubnetMask(length));
     }
 }
--- src/Router.Domain/SubnetMask.cs
+++ src/Router.Domain/SubnetMask.cs
@@ -9,6 +9,9 @@ public class SubnetMask
 
     public SubnetMask(int maskLength)
     {
+        if (maskLength is < 0 or > 32)
+            throw new ArgumentOutOfRangeException(nameof(maskLength), maskLength,
+                                                  "Mask length should be positive and less or equal to 32");
         MaskLength = maskLength;
         IpAddress = new IPAddress(0);
     }
