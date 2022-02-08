7bc9662e21aea1f9d17133fbdd8547564c6c5416
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-08 09:11:12 +0300

    chore: Add null check in constructor for LanParameters

--- src/Router.Domain/RouterProperties/LanParameters.cs
+++ src/Router.Domain/RouterProperties/LanParameters.cs
@@ -6,9 +6,9 @@ public struct LanParameters
 {
     public LanParameters(MacAddress macAddress, IPAddress ipAddress, SubnetMask subnetMask)
     {
-        MacAddress = macAddress;
-        IpAddress = ipAddress;
-        SubnetMask = subnetMask;
+        MacAddress = macAddress ?? throw new ArgumentNullException(nameof(macAddress));
+        IpAddress = ipAddress ?? throw new ArgumentNullException(nameof(ipAddress));
+        SubnetMask = subnetMask ?? throw new ArgumentNullException(nameof(subnetMask));
     }
     
     public MacAddress MacAddress { get; }
