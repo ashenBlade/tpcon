a3b45fd5748586a051226bf2fda8cbacf18f7e65
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-08 21:02:47 +0300

    chore: Add CheckConnectionAsync implementation to TPLinkRouter

--- src/Router.TPLink/TPLinkRouter.cs
+++ src/Router.TPLink/TPLinkRouter.cs
@@ -1,4 +1,6 @@
-﻿using Router.Domain;
+﻿using System.Net.Http.Headers;
+using System.Text;
+using Router.Domain;
 using Router.TPLink.JsParser;
 using Router.TPLink.ScriptExtractor;
 
@@ -7,11 +9,34 @@ namespace Router.TPLink;
 public abstract class TPLinkRouter : RemoteRouter
 {
     protected IJsParser JsParser { get; }
-    protected IScriptExtractor ScriptExtractor { get;  }
-    protected TPLinkRouter(string username, string password, Uri address, IJsParser jsParser, IScriptExtractor scriptExtractor) 
+    protected IScriptExtractor ScriptExtractor { get; }
+    protected HttpClient HttpClient { get; }
+    protected TPLinkRouter(string username, string password, Uri address, IJsParser jsParser, IScriptExtractor scriptExtractor, HttpClient httpClient) 
         : base(username, password, address)
     {
         JsParser = jsParser;
         ScriptExtractor = scriptExtractor;
+        HttpClient = httpClient;
+    }
+
+    private string? _hashedCredentials;
+
+    private string HashedCredentials =>
+        _hashedCredentials ??= Convert.ToBase64String(Encoding.UTF8.GetBytes($"{Username}:{Password}"));
+    public override async Task<bool> CheckConnectionAsync()
+    {
+        var message = new HttpRequestMessage() {Method = HttpMethod.Get, RequestUri = Address};
+        message.Headers.Authorization = new AuthenticationHeaderValue("Basic", HashedCredentials);
+        try
+        {
+            var response = await HttpClient.SendAsync(message);
+            response.EnsureSuccessStatusCode();
+            return true;
+        }
+        
+        catch (HttpRequestException ex)
+        {
+            return false;
+        }
     }
 }
