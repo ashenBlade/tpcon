967f79c9fe01c2ea43c5062ca25e4a2b9658e7ed
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-08 09:03:13 +0300

    chore: Add modifier methods to create new instances
    With* methods create new instance with new values and same not modified

--- src/Router.Domain/RouterProperties/WlanParameters.cs
+++ src/Router.Domain/RouterProperties/WlanParameters.cs
@@ -1,14 +1,30 @@
 namespace Router.Domain.RouterProperties;
 
-public struct WlanParameters
+public class WlanParameters
 {
     public WlanParameters(string ssid, string password, bool isActive)
     {
-        SSID = ssid;
-        Password = password;
+        SSID = ssid ?? throw new ArgumentNullException(nameof(ssid));
+        Password = password ?? throw new ArgumentNullException(nameof(password));
         IsActive = isActive;
     }
-    public string SSID { get;  }
+
+    public string SSID { get; }
     public string Password { get; }
     public bool IsActive { get; }
+
+    public WlanParameters WithPassword(string newPassword)
+    {
+        return new WlanParameters(SSID, newPassword ?? throw new ArgumentNullException(nameof(newPassword)), IsActive);
+    }
+
+    public WlanParameters WithSSID(string newSSID)
+    {
+        return new WlanParameters(newSSID ?? throw new ArgumentNullException(nameof(newSSID)), Password, IsActive);
+    }
+
+    public WlanParameters WithActiveState(bool isActive)
+    {
+        return new WlanParameters(SSID, Password, isActive);
+    }
 }
