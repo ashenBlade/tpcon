45dc5e3e071946e40a2276f8c343ebbbb01972f7
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-08 08:32:04 +0300

    chore: Add ToString overloading for SubnetMask
    Add tests and implementation

--- Router.Domain.Tests/SubnetMaskTests.cs
+++ Router.Domain.Tests/SubnetMaskTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Net;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Router.Domain.Tests;
 
@@ -40,5 +42,38 @@ public class SubnetMaskTests
     {
         var length = Random.Shared.Next(33, int.MaxValue);
         Assert.Throws<ArgumentOutOfRangeException>(() => new SubnetMask(length));
+    }
+
+    [Fact]
+    public void ToString_WithLength4_ShouldReturn240dot0dot0dot0()
+    {
+        // Arrange
+        var expected = "240.0.0.0";
+        var mask = new SubnetMask(4);
+        
+        // Act
+        var actual = mask.ToString();
+        
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(1, "128.0.0.0")]
+    [InlineData(32, "255.255.255.255")]
+    [InlineData(16, "255.255.0.0")]
+    [InlineData(24, "255.255.255.0")]
+    [InlineData(0, "0.0.0.0")]
+    [InlineData(26, "255.255.255.192")]
+    [InlineData(20, "255.255.240.0")]
+    public void ToString_WithValidLengthInConstructor_ShouldReturnValidStringIPRepresentation(int length, string expected)
+    {
+        // Arrange
+        var mask = new SubnetMask(length);
+
+        // Act
+        var actual = mask.ToString();
+        // Assert
+        Assert.Equal(expected, actual);
     }
 }
--- src/Router.Domain/SubnetMask.cs
+++ src/Router.Domain/SubnetMask.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Net;
 
 namespace Router.Domain;
@@ -13,6 +14,52 @@ public class SubnetMask
             throw new ArgumentOutOfRangeException(nameof(maskLength), maskLength,
                                                   "Mask length should be positive and less or equal to 32");
         MaskLength = maskLength;
-        IpAddress = new IPAddress(0);
+        IpAddress = new IPAddress(GetBytesFromLength(MaskLength));
+    }
+
+    private const byte MaskZero = 0b0000_0000;
+    private const byte MaskOne = 0b1000_0000;
+    private const byte MaskTwo = 0b1100_0000;
+    private const byte MaskThree = 0b1110_0000;
+    private const byte MaskFour = 0b1111_0000;
+    private const byte MaskFive = 0b1111_1000;
+    private const byte MaskSix = 0b1111_1100;
+    private const byte MaskSeven = 0b1111_1110;
+    private const byte MaskEight = 0b1111_1111;
+
+    private static readonly byte[] MaskForLength =
+    {
+        MaskZero, MaskOne, MaskTwo, MaskThree, MaskFour, MaskFive, MaskSix, MaskSeven, MaskEight
+    };
+    
+    private static byte[] GetBytesFromLength(int length)
+    {
+        const int bytesInMask = 4;
+        var bytes = new byte[bytesInMask];
+        var full = length / 8;
+        var semi = length % 8;
+        var i = 0;
+        for (; i < full; i++)
+        {
+            bytes[i] = MaskEight;
+        }
+
+        if (i < bytesInMask)
+        {
+            bytes[i] = MaskForLength[semi];
+            i++;
+        }
+
+        for (; i < bytesInMask; i++)
+        {
+            bytes[i] = MaskZero;
+        }
+
+        return bytes;
+    }
+
+    public override string ToString()
+    {
+        return IpAddress.ToString();
     }
 }
