33975d47084091ab48aa98b1b96264db2388469b
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-09 11:34:24 +0300

    chore: Fix regex for html script tags

--- src/JsUtils.Implementation/HtmlScriptExtractor.cs
+++ src/JsUtils.Implementation/HtmlScriptExtractor.cs
@@ -1,9 +1,22 @@
-﻿namespace JsUtils.Implementation;
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace JsUtils.Implementation;
 
 public class HtmlScriptExtractor : IScriptExtractor
 {
     public string ExtractScript(string source)
     {
-        return string.Empty;
+        var regex = new Regex("<script(.|\n)*?>(?<Content>(.|\n)*?)</script>");
+        var matches = regex.Matches(source);
+        var scripts = new List<string>();
+        foreach (Match match in matches)
+        {
+            scripts.Add(match.Groups["Content"].Value);
+        }
+
+        return scripts.Count == 0 
+                   ? string.Empty 
+                   : scripts.Aggregate((s, n) => $"{s}\n{n}");
     }
 }
