a06a1c7e4037e49837cde5db700cd0c1f82b8d57
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-11 19:31:26 +0300

    chore: Add variable extracting from token sequence

--- JsUtils.Implementation.Tests/JsVariableExtractorTests.cs
+++ JsUtils.Implementation.Tests/JsVariableExtractorTests.cs
@@ -1,33 +1,53 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using JsTypes;
+using JsUtils.Implementation.JsTokens;
+using Moq;
 using Xunit;
+using JsType = JsUtils.Implementation.JsTokens.JsType;
 
 namespace JsUtils.Implementation.Tests;
 
 public class JsVariableExtractorTests
 {
-    private readonly JsVariableExtractor _jsVariableExtractor = new(new Tokenizer());
-    private List<JsVariable> Parse(string script) => _jsVariableExtractor.ExtractVariables(script).ToList();
-
-    [Theory]
-    [InlineData("")]
-    [InlineData("\t\t")]
-    [InlineData("\n\n\t")]
-    [InlineData("    ")]
-    public void ParseScript_WithEmptyOrWhiteSpacedString_ShouldReturnEmptyScript(string whitespace)
+    private static ITokenizer GetTokenizer(params JsToken[] toReturn)
     {
-        var result = Parse(whitespace);
+        var mock = new Mock<ITokenizer>();
+        mock.Setup(tokenizer => tokenizer.Tokenize(Moq.It.IsAny<string>())).Returns(toReturn);
+        return mock.Object;
+    }
+
+    private static IEnumerable<JsVariable> Parse(JsToken[] tokens)
+    {
+        return new JsVariableExtractor(GetTokenizer(tokens)).ExtractVariables("");
+    }
+
+    [Fact]
+    public void ParseScript_WithNoTokens_ShouldReturnEmptyScript()
+    {
+        var result = Parse(Array.Empty<JsToken>());
         Assert.Empty(result);
     }
 
+
+    public readonly static IEnumerable<object[]> SingleVariableAssignments = new[]
+                                                                             {
+                                                                                 new JsToken[]
+                                                                                 {
+                                                                                     new JsIdentifier("x"),
+                                                                                     new JsTokens.JsNumber(10)
+                                                                                 }
+                                                                             };
+
     [Theory]
-    [InlineData("var x = 10;", "x")]
-    [InlineData("var date = Date.now();", "date")]
-    [InlineData("var value = \"Hello, world!\"", "value")]
-    public void ParseScript_WithOnlyOneVariableDeclaration_ShouldReturnScriptWithOnlyOneVariable(string singleVariableDeclarationScriptString, string variableName)
+    [MemberData(nameof(SingleVariableAssignments))]
+    public void ExtractVariables_WithSingleVariableDeclarationAndAssignment_ShouldReturnOneVariable(JsIdentifier identifier, JsType type)
     {
-        var variables = Parse(singleVariableDeclarationScriptString);
-        Assert.Contains(variables, variable => variable.Name == variableName);
+        var actual = Parse(new JsToken[] {new JsVar(), identifier, new JsEquals(), type, new JsSemicolon()}).ToList();
+        Assert.Single(actual);
+        var variable = actual[0];
+        Assert.Equal(identifier.Name, variable.Name);
+        Assert.Equal(type.ToJsType(), variable.Value);
     }
 }
--- src/JsUtils.Implementation/JsVariableExtractor.cs
+++ src/JsUtils.Implementation/JsVariableExtractor.cs
@@ -1,4 +1,8 @@
+using System.Collections;
+
 using JsTypes;
+using JsUtils.Implementation.JsTokens;
+using JsType = JsUtils.Implementation.JsTokens.JsType;
 
 namespace JsUtils.Implementation;
 
@@ -13,11 +17,104 @@ public class JsVariableExtractor : IJsVariableExtractor
     
     public IEnumerable<JsVariable> ExtractVariables(string script)
     {
-        return FindVariablesInScript(script);
+        using var extractor = new VariableStatementExtractor(Tokenizer.Tokenize(script));
+        return extractor.ToList();
     }
 
-    private IEnumerable<JsVariable> FindVariablesInScript(string script)
+    private class VariableStatementExtractor : IDisposable, IEnumerable<JsVariable>
     {
-        return Array.Empty<JsVariable>();
+        public IEnumerator<JsToken> JsTokenEnumerator { get; }
+        public VariableStatementExtractor(IEnumerable<JsToken> jsTokens)
+        {
+            JsTokenEnumerator = jsTokens.GetEnumerator();
+        }
+
+        public void Dispose()
+        {
+            JsTokenEnumerator?.Dispose();
+        }
+        
+        public IEnumerator<JsVariable> GetEnumerator()
+        {
+            while (JsTokenEnumerator.MoveNext())
+            {
+                var token = JsTokenEnumerator.Current;
+                if (token is JsVar)
+                {
+                    yield return ReadVariable();
+                }
+                else
+                {
+                    MoveNext();
+                }
+            }
+        }
+
+        private bool MoveNext() => JsTokenEnumerator.MoveNext();
+        private JsToken Current => JsTokenEnumerator.Current;
+
+        private JsVariable ReadVariable()
+        {
+            ReadVar();
+            var id = ReadIdentifier();
+            ReadEquals();
+            var type = ReadType();
+            ReadSemicolon();
+            return new JsVariable(id.Name, type.ToJsType());
+        }
+
+        private void ReadSemicolon()
+        {
+            if (Current is not JsSemicolon)
+                throw new Exception($"Expected ';' got {Current.Name}");
+            MoveNext();
+        }
+
+        private JsType ReadType()
+        {
+            if (Current is not JsType type)
+            {
+                throw new Exception($"Expected JsType(number, string, regex) got {Current.Name}");
+            }
+
+            MoveNext();
+            return type;
+        }
+
+        private JsEquals ReadEquals()
+        {
+            if (Current is not JsEquals equals)
+            {
+                throw new Exception($"Expected '=' got {Current.Name}");
+            }
+
+            MoveNext();
+            return equals;
+        }
+
+        private JsIdentifier ReadIdentifier()
+        {
+            if (Current is not JsIdentifier identifier)
+            {
+                throw new Exception($"Expected identifier got {Current.Name}");
+            }
+            
+            MoveNext();
+            return new JsIdentifier(identifier.Name);
+        }
+
+        private void ReadVar()
+        {
+            if (Current is not JsVar)
+            {
+                throw new Exception($"Expected 'var' got '{Current.Name}'");
+            }
+            MoveNext();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
