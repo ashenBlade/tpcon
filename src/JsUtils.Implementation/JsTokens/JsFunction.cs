075099ddf23878ca6addd7d2142dd1f5c6962610
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-12 11:46:41 +0300

    chore: Add 'function' token type
    Add:
    - JsFunction class
    - Function enum to JsTokenType

---
+++ src/JsUtils.Implementation/JsTokens/JsFunction.cs
@@ -0,0 +1,6 @@
+namespace JsUtils.Implementation.JsTokens;
+
+public class JsFunction : JsToken
+{
+    public JsFunction() : base("function", JsTokenType.Function) { }
+}
--- src/JsUtils.Implementation/JsTokens/JsTokenType.cs
+++ src/JsUtils.Implementation/JsTokens/JsTokenType.cs
@@ -21,5 +21,6 @@ public enum JsTokenType
     For,
     New,
     LeftSquareBracket,
-    RightSquareBracket
+    RightSquareBracket,
+    Function
 }
