cdb272dab820187ce80559c25def7d886186d729
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-17 14:32:59 +0300

    chore: Add Tags class
    Contains constants for basic tags for Token type

---
+++ src/JsUtils.Implementation/Tokens/Tags.cs
@@ -0,0 +1,17 @@
+namespace JsUtils.Implementation.Tokens;
+
+public static class Tags
+{
+    public const int And = 256,
+                     Else = 257,
+                     For = 258,
+                     While = 259,
+                     Switch = 260,
+                     Case = 261,
+                     Do = 262,
+                     Id = 263,
+                     Number = 264,
+                     String = 265,
+                     Regex = 266,
+                     Bool = 267;
+}
