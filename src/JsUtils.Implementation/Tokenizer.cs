5bfabbe37bf98d69e24e08e56de87a7d36d41284
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-17 14:53:36 +0300

    chore: Add Tokenizer class
    Implements ITokenizer.
     Tokenize throws NotImplementedException always

---
+++ src/JsUtils.Implementation/Tokenizer.cs
@@ -0,0 +1,11 @@
+using JsUtils.Implementation.Tokens;
+
+namespace JsUtils.Implementation;
+
+public class Tokenizer : ITokenizer
+{
+    public IEnumerable<Token> Tokenize(string source)
+    {
+        throw new NotImplementedException();
+    }
+}
