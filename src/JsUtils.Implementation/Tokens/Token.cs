b4e00faadf7b0692bc538f3c42dab3da9c53bce9
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-17 14:33:46 +0300

    chore: Add comparison for Tokens

--- src/JsUtils.Implementation/Tokens/Token.cs
+++ src/JsUtils.Implementation/Tokens/Token.cs
@@ -1,11 +1,31 @@
 namespace JsUtils.Implementation.Tokens;
 
-public class Token
+public class Token: IEquatable<Token>
 {
     public int Tag { get; }
 
     public Token(int tag)
     {
         Tag = tag;
+    }
+
+    public bool Equals(Token? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Tag == other.Tag;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals(( Token ) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Tag;
     }
 }
