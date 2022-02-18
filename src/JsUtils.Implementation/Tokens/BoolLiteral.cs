62778f3c1b4c31a688155a24752fe31b2a2336fa
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-18 10:30:28 +0300

    chore: Add readonly True and False to BoolLiteral

--- src/JsUtils.Implementation/Tokens/BoolLiteral.cs
+++ src/JsUtils.Implementation/Tokens/BoolLiteral.cs
@@ -2,6 +2,9 @@ namespace JsUtils.Implementation.Tokens;
 
 public class BoolLiteral: Literal<bool>
 {
+    public static readonly BoolLiteral True = new(true);
+
+    public static readonly BoolLiteral False = new(false);
     public BoolLiteral(bool value) : base(value, value ? Tags.True : Tags.False)
     { }
 }
