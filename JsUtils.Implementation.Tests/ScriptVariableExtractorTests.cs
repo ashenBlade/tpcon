3de7125f656093d091585a1b9344e7baa77d655e
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-24 09:44:46 +0300

    Add test for empty token sequence

--- JsUtils.Implementation.Tests/ScriptVariableExtractorTests.cs
+++ JsUtils.Implementation.Tests/ScriptVariableExtractorTests.cs
@@ -1,5 +1,8 @@
+using System.Linq;
+using JsTypes;
 using JsUtils.Implementation.Tokens;
 using Moq;
+using Xunit;
 
 namespace JsUtils.Implementation.Tests;
 
@@ -17,5 +20,12 @@ public class ScriptVariableExtractorTests
     }
 
     private ScriptVariableExtractor GetExtractor(params Token[] tokens) => new(GetTokenizer(tokens));
-
+    
+    [Fact]
+    public void ExtractVariables_WithEmptySequence_ShouldReturnEmptySequence()
+    {
+        var extractor = GetExtractor();
+        var actual = extractor.ExtractVariables(string.Empty).ToList();
+        Assert.Empty(actual);
+    }
 }
