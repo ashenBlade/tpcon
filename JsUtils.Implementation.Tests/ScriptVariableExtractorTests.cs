43f649c9141c9a8f89186c8f6b38c59f712dfa5a
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-24 09:33:54 +0300

    Add helper functions for ScriptVariableExtractorTests

--- JsUtils.Implementation.Tests/ScriptVariableExtractorTests.cs
+++ JsUtils.Implementation.Tests/ScriptVariableExtractorTests.cs
@@ -1,8 +1,21 @@
+using JsUtils.Implementation.Tokens;
+using Moq;
+
 namespace JsUtils.Implementation.Tests;
 
 public class ScriptVariableExtractorTests
 {
     private ScriptVariableExtractor Extractor => new(new Tokenizer());
     private ScriptVariableExtractor GetExtractor(ITokenizer tokenizer) => new(tokenizer);
-    
+    private ITokenizer Tokenizer => new Tokenizer();
+    private ITokenizer GetTokenizer(params Token[] tokens)
+    {
+        var mock = new Mock<ITokenizer>();
+        mock.Setup(t => t.Tokenize(It.IsAny<string>()))
+            .Returns(tokens);
+        return mock.Object;
+    }
+
+    private ScriptVariableExtractor GetExtractor(params Token[] tokens) => new(GetTokenizer(tokens));
+
 }
