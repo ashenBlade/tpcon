a3f47a8971cb53ce4a565df86035b2b887c88aae
ashblade <htcdnsnnkom2002@gmail.com>
2022-02-24 21:09:00 +0300

    Add equality overriding for JsVariable
    Add IEquatable<JsVariable> interface, and override equality methods

--- src/JsTypes/JsVariable.cs
+++ src/JsTypes/JsVariable.cs
@@ -1,6 +1,6 @@
 namespace JsTypes;
 
-public class JsVariable : ICloneable
+public class JsVariable : ICloneable, IEquatable<JsVariable>
 {
     private JsType _value;
     public string Name { get; }
@@ -19,5 +19,26 @@ public class JsVariable : ICloneable
     public object Clone()
     {
         return new JsVariable(Name, ( JsType ) _value.Clone());
+    }
+
+    public bool Equals(JsVariable? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return _value.Equals(other._value)
+            && Name == other.Name;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals(( JsVariable ) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_value, Name);
     }
 }
