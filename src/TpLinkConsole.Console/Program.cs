932745235d7a24e15227b3e4668777f8d226a596
ashblade <htcdnsnnkom2002@gmail.com>
2022-03-30 11:17:07 +0300

    Add command parsing from input

--- src/TpLinkConsole.Console/Program.cs
+++ src/TpLinkConsole.Console/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Application is not implemented yet");
-Console.WriteLine("Check updates on GitHub");
+﻿using System.Runtime.CompilerServices;
+using TpLinkConsole.CommandLine;
+using TpLinkConsole.CommandLine.Exceptions;
+
+var parser = new CommandLineArgumentsParser();
+try
+{
+    var cmd = parser.Parse(args);
+}
+catch (ArgumentValueExpectedException argumentValueExpectedException)
+{
+    Console.WriteLine($"Value expected for parameter: {argumentValueExpectedException.ParameterName}");
+    return;
+}
+catch (ArgumentExpectedException argumentExpectedException)
+{
+    Console.WriteLine($"Argument expected");
+    return;
+}
+catch (CommandLineParsingException cmdParsingException)
+{
+    Console.WriteLine("Invalid arguments");
+    return;
+}
