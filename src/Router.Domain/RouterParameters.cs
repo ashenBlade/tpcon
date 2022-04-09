857f19d69cc12acc65e0347c2be468cde6824034
ashblade <htcdnsnnkom2002@gmail.com>
2022-04-09 16:00:49 +0300

    [refactor] Rename IpAddress to Address for RouterParameters

--- src/Router.Domain/RouterParameters.cs
+++ src/Router.Domain/RouterParameters.cs
@@ -4,7 +4,7 @@ namespace Router.Domain;
 
 public struct RouterParameters
 {
-    public IPAddress IpAddress { get; set; } = new IPAddress(new byte[] {192, 168, 0, 1});
+    public IPAddress Address { get; set; } = new IPAddress(new byte[] {192, 168, 0, 1});
     public string Username { get; set; } = "admin";
     public string Password { get; set; } = "admin";
 
--- src/Router.Domain/RouterParametersExtensions.cs
+++ src/Router.Domain/RouterParametersExtensions.cs
@@ -4,6 +4,6 @@ public static class RouterParametersExtensions
 {
     public static Uri Address(this RouterParameters parameters)
     {
-        return new Uri($"http://{parameters.IpAddress}");
+        return new Uri($"http://{parameters.Address}");
     }
 }
