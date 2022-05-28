176f60812ecb7c1d9b185caa35e20232ae35a2c2
ash.blade <ashen.blade.dev@gmail.com>
2022-05-28 14:41:01 +0300

    fix: add stub implementations for FakeTpLinkRouter in Tests

--- tests/Router.TpLink.Tests/Mocks/FakeTpLinkRouter.cs
+++ tests/Router.TpLink.Tests/Mocks/FakeTpLinkRouter.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Router.Domain;
 
 namespace Router.TpLink.Tests.Mocks;
@@ -5,6 +6,15 @@ namespace Router.TpLink.Tests.Mocks;
 public class FakeTpLinkRouter : TpLinkRouter
 {
     public new RouterParameters RouterParameters { get; }
+    public override Task RefreshAsync()
+    {
+        throw new MustNotBeCalledException();
+    }
+
+    public override Task<bool> CheckConnectionAsync()
+    {
+        throw new MustNotBeCalledException();
+    }
 
     public FakeTpLinkRouter(RouterParameters routerParameters) : base(new FakeRouterHttpMessageSender(), RouterParameters.Default,  new FakeLanConfigurator(), new FakeWlanConfigurator())
     {
